3113d4f8f09b4f2ae6aea434e151e5f78c839610
Omid Talwar <omidtalwar.official@gmail.com>
2026-05-07 15:43:00 +0430

    home page

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 if (!empty($_SESSION['user_id'])) {
-    header('Location: /fzl/dashboard.php');
+    header('Location: /dashboard.php');
 } else {
     header('Location: /fzl/home.php');
 }
